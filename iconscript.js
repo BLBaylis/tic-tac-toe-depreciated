acfe8eb4aa8dc4632dfe5d592a1df079dbf4f4f2
BLBaylis <bradleybaylis@hotmail.com>
2018-03-03 21:57:12 +0000

    Added computer icon select screen

--- iconscript.js
+++ iconscript.js
@@ -1,19 +1,49 @@
 document.addEventListener("DOMContentLoaded", function() {
+	document.getElementsByClassName("confirm-btn")[0].addEventListener("click", flip);
+	document.getElementsByClassName("back-btn")[0].addEventListener("click", flip);
 	for (var i = 0; i < document.getElementsByClassName("icon").length; i++){
-		document.getElementsByClassName("icon")[i].addEventListener("click", function(event){
-			playerIcon.icon = event.target.classList[1];
-			document.getElementsByClassName("canvas-inner")[0].style.backgroundImage = "url('icons/"
-			 + playerIcon.icon + "/" + playerIcon.icon + " " + playerIcon.colour + ".png')";
-		})
+		document.getElementsByClassName("icon")[i].addEventListener("click", iconSelect);
 	}
 	for (var i = 0; i < document.getElementsByClassName("colour").length; i++){
-		document.getElementsByClassName("colour")[i].addEventListener("click", function(event){
-			playerIcon.colour = event.target.classList[1];
-			document.getElementsByClassName("canvas-inner")[0].style.backgroundImage = "url('icons/"
-			 + playerIcon.icon + "/" + playerIcon.icon + " " + playerIcon.colour + ".png')";
-		})
+		document.getElementsByClassName("colour")[i].addEventListener("click", colourSelect);
 	}
 });
 
-var playerIcon = {colour : "black", icon : "circle"},
-computerIcon = {colour : "black", icon : "cross"};
+var userIcon = {colour : "black", icon : "circle"},
+computerIcon = {colour : "black", icon : "cross"},
+flipped = false;
+
+function flip() {
+  if (!flipped){
+    document.getElementsByClassName("flipper")[0].classList.add("go-flip");
+  } else if (flipped) {
+    document.getElementsByClassName("flipper")[0].classList.remove("go-flip");
+  }
+  flipped = !flipped;
+}
+
+function iconSelect(event){
+	var whichPlayer = event.target.classList[0];
+	if (whichPlayer === "user"){
+		userIcon.icon = event.target.classList[2];
+		document.getElementsByClassName("user-canvas-inner")[0].style.backgroundImage = "url('icons/" + userIcon.icon + "/" + userIcon.icon 
+		+ " " + userIcon.colour + ".png')";
+	} else if (whichPlayer === "computer"){
+		computerIcon.icon = event.target.classList[2];
+		document.getElementsByClassName("computer-canvas-inner")[0].style.backgroundImage = "url('icons/"
+		+ computerIcon.icon + "/" + computerIcon.icon + " " + computerIcon.colour + ".png')";
+	}
+}
+
+function colourSelect(event){
+	var whichPlayer = event.target.classList[0];
+	if (whichPlayer === "user"){
+		userIcon.colour = event.target.classList[2];
+		document.getElementsByClassName("user-canvas-inner")[0].style.backgroundImage = "url('icons/"
+		+ userIcon.icon + "/" + userIcon.icon + " " + userIcon.colour + ".png')";
+	} else if (whichPlayer === "computer"){
+		computerIcon.colour = event.target.classList[2];
+		document.getElementsByClassName("computer-canvas-inner")[0].style.backgroundImage = "url('icons/"
+		+ computerIcon.icon + "/" + computerIcon.icon + " " + computerIcon.colour + ".png')";
+	}
+}
